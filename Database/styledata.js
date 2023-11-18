54ea93fedd40c35cb8913d92f84726d88c5ca96c
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-11-18 12:07:41 +0530

    Version 1.0

---
+++ Database/styledata.js
@@ -0,0 +1,28 @@
+const colorLib = [
+       {
+       	id: 0,
+           theme: "Dark",
+           collist: [
+                  ['--bg-color', '#000000ec'],
+                  ['--nv-color', '#343a40'],
+                  ['--wtxt-color', '#fff'],
+                  ['--blink-color', 'antiquewhite'],
+                  ['--nvtxt-color', '#cfd2d6'],
+                  ['--ngtxt-color', '#00ff09'],
+                  ['--sg-color', '#28a745']
+            ]
+       },
+       {
+       	id: 1,
+           theme: "Light",
+           collist: [
+                ['--bg-color', '#f6f6f6'],
+                ['--nv-color', '#0c8ff0ec'],
+                ['--wtxt-color', '#000'],
+                ['--blink-color', 'brown'],
+                ['--nvtxt-color', '#fff'],
+                ['--ngtxt-color', '#0c8ff0'],
+                ['--sg-color', '#0c8ff9']
+           ]
+       }
+];
