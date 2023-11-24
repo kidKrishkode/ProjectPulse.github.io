a4ced2644c237707610ebab905909d147a94edac
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-11-24 19:35:47 +0530

    Version 1.0

--- Database/styledata.js
+++ Database/styledata.js
@@ -9,7 +9,8 @@ const colorLib = [
                   ['--blink-color', 'antiquewhite'],
                   ['--nvtxt-color', '#cfd2d6'],
                   ['--ngtxt-color', '#00ff09'],
-                  ['--sg-color', '#28a745']
+                  ['--sg-color', '#28a745'],
+                  ['--side-l-color', '#000']
             ]
        },
        {
@@ -22,7 +23,34 @@ const colorLib = [
                 ['--blink-color', 'brown'],
                 ['--nvtxt-color', '#fff'],
                 ['--ngtxt-color', '#0c8ff0'],
-                ['--sg-color', '#0c8ff9']
+                ['--sg-color', '#0c8ff9'],
+                ['--side-l-color', '#000080cf']
            ]
        }
+];
+const KrishSlider = [
+    {
+        id: 0,
+        src: "./img/ksm001.png"
+    },
+    {
+        id: 1,
+        src: "./img/ksm002.jpg"
+    },
+    {
+        id: 2,
+        src: "./img/ksm003.jpg"
+    },
+    {
+        id: 3,
+        src: "./img/ksm004.jpg"
+    },
+    {
+        id: 4,
+        src: "./img/ksm005.jpg"
+    },
+    {
+        id: 5,
+        src: "./img/ksm006.jpg"
+    },
 ];
